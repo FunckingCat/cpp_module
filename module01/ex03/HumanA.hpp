577295fce116d4e1f99a7b5323e50b2231ea75e7
David Zhuravlev <david99111@mial.ru>
2022-01-17 20:55:17 +0300

    started_ex03

---
+++ module01/ex03/HumanA.hpp
@@ -0,0 +1,16 @@
+#ifndef HUMANA_HPP
+#define HUMANA_HPP
+
+#include <iostream>
+#include <string>
+
+class HumanA
+{
+private:
+
+public:
+	HumanA();
+	~HumanA();
+};
+
+#endif
---
+++ module01/ex03/Weapon.hpp
@@ -0,0 +1,17 @@
+#ifndef WEAPON_HPP
+#define WEAPON_HPP
+
+#include <iostream>
+#include <string>
+
+class Weapon
+{
+private:
+
+public:
+	Weapon();
+	~Weapon();
+};
+
+
+#endif
