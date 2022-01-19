6700b4e146c3cb4f4b192d9f4ac33d3e82a06bae
David Zhuravlev <david99111@mial.ru>
2022-01-19 13:24:06 +0300

    ex01

--- module02/ex01/Fixed.cpp
+++ module02/ex01/Fixed.cpp
@@ -7,6 +7,18 @@ Fixed::Fixed(): fixed_val(0)
 	std::cout << "Default constructor called\n";
 }
 
+Fixed::Fixed(int const val)
+{
+	this->fixed_val = val << Fixed::raw;
+	std::cout << "Int constructor called\n";
+}
+
+Fixed::Fixed(float const val)
+{
+	this->fixed_val = roundf(val * (1 << Fixed::raw));
+	std::cout << "Float constructor called\n";
+}
+
 Fixed::Fixed(const Fixed &old)
 {
 	this->fixed_val = old.getRawBits();
@@ -35,4 +47,20 @@ void Fixed::setRawBits( int const raw )
 {
 	std::cout << "setRawBits member function called\n";
 	this->fixed_val = raw;
+}
+
+float Fixed::toFloat( void ) const
+{
+	return ((float)this->fixed_val/(float)(1 << Fixed::raw));
+}
+
+int Fixed::toInt( void ) const
+{
+	return (this->fixed_val >> Fixed::raw);
+}
+
+std::ostream &operator<<(std::ostream &out, Fixed const &value)
+{
+	out << value.toFloat();
+	return (out);
 }
---
+++ module02/ex01/Fixed.hpp
@@ -0,0 +1,23 @@
+#include <iostream>
+# include <cmath>
+
+class Fixed
+{
+private:
+	static const int raw;
+	int fixed_val;
+public:
+	Fixed();
+	Fixed(int const val);
+	Fixed(float const val);
+	Fixed(const Fixed &old);
+	~Fixed();
+
+	Fixed operator= (const Fixed &old);
+	int getRawBits( void ) const;
+	void setRawBits( int const raw );
+	float toFloat( void ) const;
+	int toInt( void ) const;
+};
+
+std::ostream &operator<<(std::ostream &out, Fixed const &value);
