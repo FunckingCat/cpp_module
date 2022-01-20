b3fa416885df170450536d0d074b1eace93fb1cd
David Zhuravlev <david99111@mial.ru>
2022-01-20 19:51:26 +0300

    init2

--- module04/ex03/IMateriaSource.hpp
+++ module04/ex03/IMateriaSource.hpp
@@ -1,4 +1,17 @@
 #ifndef IMATERIA_HPP
 #define IMATERIA_HPP
 
+#include <iostream>
+#include <string>
+
+#include "AMateria.hpp"
+
+class IMateriaSource
+{
+	public:
+	virtual ~IMateriaSource() {}
+	virtual void learnMateria(AMateria*) = 0;
+	virtual AMateria* createMateria(std::string const & type) = 0;
+};
+
 #endif
