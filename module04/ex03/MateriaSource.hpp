97d502d08b8ca9899f88662cb82751ce29d2ce96
David Zhuravlev <david99111@mial.ru>
2022-01-20 20:50:08 +0300

    stage

--- module04/ex03/MateriaSource.hpp
+++ module04/ex03/MateriaSource.hpp
@@ -6,6 +6,22 @@
 
 #include "IMateriaSource.hpp"
 
+#define	INV_SZ 4
 
+class MateriaSource: public IMateriaSource
+{
+private:
+	int amount;
+	AMateria *sources[INV_SZ];
+public:
+	MateriaSource();
+	MateriaSource(MateriaSource const &other);
+	virtual ~MateriaSource();
+
+	MateriaSource &operator=(MateriaSource const &other);
+
+	void learnMateria(AMateria *m);
+	AMateria* createMateria(std::string const &type);
+};
 
 #endif
