456bb08e86c2532f78e7eb5adfa67e8eab2e6a45
David Zhuravlev <david99111@mial.ru>
2022-01-20 12:38:08 +0300

    ex00

---
+++ module04/ex00/main.cpp
@@ -0,0 +1,18 @@
+#include "Animal.hpp"
+#include "Cat.hpp"
+#include "Dog.hpp"
+
+int main()
+{
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	meta->makeSound();
+	std::cout << j->getType() << " " << std::endl;
+	j->makeSound();
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound();
+	delete(meta);
+	delete(j);
+	delete(i);
+}
