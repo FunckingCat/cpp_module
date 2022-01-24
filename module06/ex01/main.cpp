6607cc28d0a91a6d8f322b343fc6c8991052caa8
David Zhuravlev <david99111@mial.ru>
2022-01-24 18:04:09 +0300

    ex01

---
+++ module06/ex01/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+typedef struct s_Data
+{
+	std::string str;
+} Data;
+
+uintptr_t serialize(Data* ptr)
+{
+	return (reinterpret_cast<uintptr_t>(ptr));
+}
+
+Data* deserialize(uintptr_t raw)
+{
+	return (reinterpret_cast<Data *>(raw));
+}
+
+int main(void)
+{
+	Data *data = new Data;
+	Data *after;
+	uintptr_t ptr;
+	data->str = "Serialization";
+	std::cout << data << " : " << data->str << std::endl;
+
+	ptr = serialize(data);
+	after = deserialize(ptr);
+
+	std::cout << after << " : " << after->str << std::endl;
+	return (0);
+}
