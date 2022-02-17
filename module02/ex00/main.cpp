93df8efbf0b9ffccc9c25faea0da5b9fca7ed0e4
David Zhuravlev <david99111@mial.ru>
2022-02-17 14:03:38 +0300

    fix

--- module02/ex00/main.cpp
+++ module02/ex00/main.cpp
@@ -4,10 +4,8 @@
 int main( void ) {
 	Fixed a;
 	Fixed b( a );
-	std::cout << std::endl;
 	Fixed c;
 	c = b;
-	std::cout << std::endl;
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
