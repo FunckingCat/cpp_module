a699b566477ba88899b659ab9ff6c3628bdad6a1
David Zhuravlev <david99111@mial.ru>
2022-02-20 11:45:27 +0300

    fix0001

--- module07/ex01/iter.hpp
+++ module07/ex01/iter.hpp
@@ -2,9 +2,16 @@
 #define ITER_HPP
 
 template<typename T>
-void iter(T *arr, size_t size, void (*f)(T const &item));
+void	iter(T *arr, size_t size, void (*f)(T const &item))
+{
+	for (size_t i = 0; i < size; i++)
+		(*f)(arr[i]);
+}
 
 template<typename T>
-void func(T const &itm);
+void func(T const &itm)
+{
+	std::cout << itm << " : ";
+}
 
 #endif
--- module07/ex01/main.cpp
+++ module07/ex01/main.cpp
@@ -1,19 +1,6 @@
 #include <iostream>
 #include "iter.hpp"
 
-template<typename T>
-void	iter(T *arr, size_t size, void (*f)(T const &item))
-{
-	for (size_t i = 0; i < size; i++)
-		(*f)(arr[i]);
-}
-
-template<typename T>
-void func(T const &itm)
-{
-	std::cout << itm << " : ";
-}
-
 int main(void)
 {
 	int intR[5] = { 0, 1, 2, 3, 4 };
@@ -32,6 +19,5 @@ int main(void)
 	iter(stringR, 5, &func); std::cout << std::endl;
 	iter(stringR, 2, &func); std::cout << std::endl;
 	iter(stringR, 0, &func); std::cout << std::endl;
-
 	return (0);
 }
